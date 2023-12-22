aecaaab83db95deb1a577cbd325b16b77245bf5c
rogerworkup <rogerworkup@gmail.com>
2023-12-21 22:42:29 -0300

    Delete a record

--- Back-End/index.js
+++ Back-End/index.js
@@ -32,6 +32,13 @@ app.put('/updateUser/:id', (req, res) => {
     .catch(err => res.json(err))
 })
 
+app.delete('/deleteUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndDelete({_id: id})
+    .then(res => res.json(res))
+    .catch(err => res.json(err))
+})
+
 app.post("/createUser", (req, res) => {
     UserModel.create(req.body)
     .then(users => res.json(users))
